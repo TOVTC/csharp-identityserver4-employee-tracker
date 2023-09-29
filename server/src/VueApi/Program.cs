1c1913e4f7ec12832c0705478db4e695397421a4
Veronica Tai Chi To <99231049+TOVTC@users.noreply.github.com>
2023-09-29 15:22:24 -0400

    attempting new template

--- server/src/VueApi/Program.cs
+++ server/src/VueApi/Program.cs
@@ -1,12 +1,7 @@
-// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-
-using System;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace Api
+namespace JavaScriptClient
 {
     public class Program
     {
