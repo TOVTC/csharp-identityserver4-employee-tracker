e63d3d7c8ac17cd2c3ba0a024931d98f8211ade1
Veronica Tai Chi To <99231049+TOVTC@users.noreply.github.com>
2023-09-29 15:21:35 -0400

    attempting new template

---
+++ server/src/VueApi/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace VueApi.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int RoleId { get; set; }
+        public int ManagerId { get; set; }
+    }
+}
---
+++ server/src/VueApi/Models/Role.cs
@@ -0,0 +1,11 @@
+﻿namespace VueApi.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string JobTitle { get; set; }
+        public int Salary { get; set; }
+        public int DeptId { get; set; }
+        public int IsManagement { get; set; }
+    }
+}
