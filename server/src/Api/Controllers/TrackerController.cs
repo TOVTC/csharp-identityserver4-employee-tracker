9ba40493e5694650bd71f0a66ca23fea415f7d8d
Veronica Tai Chi To <99231049+TOVTC@users.noreply.github.com>
2023-09-29 15:46:33 -0400

    removed global authentication requirement to selectively protect endpoints

--- server/src/Api/Controllers/TrackerController.cs
+++ server/src/Api/Controllers/TrackerController.cs
@@ -1,4 +1,5 @@
 ﻿using Api.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -7,6 +8,7 @@ namespace Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class TrackerController : ControllerBase
     {
         // GET: api/tracker/departments
--- server/src/Api/Startup.cs
+++ server/src/Api/Startup.cs
@@ -42,8 +42,7 @@ namespace Api
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers()
-                .RequireAuthorization("ApiScope");
+                endpoints.MapControllers();
             });
         }
     }
